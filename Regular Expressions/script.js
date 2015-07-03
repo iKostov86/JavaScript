13caff377d26d1af19f274c7e15b95b58470a1b2
iKostov86 <i.kostov86@gmail.com>
2015-07-03 21:37:53 +0300

    dr

--- Regular Expressions/script.js
+++ Regular Expressions/script.js
@@ -28,15 +28,52 @@ function formatWithPlaceholders() {
 }
 
 //Problem 2. HTML binding
-String.prototype.bind = function(obj) {
-    'use strict';
-    var regex = new RegExp('', 'g');
-    document.getElementsByTagName('html').innerHTML = this.replace();
-}
+//String.prototype.bind = function(obj) {
+//    'use strict';
+//    var prop,
+//        regexContent,
+//        regexLink;
+//
+//    for(prop in obj) {
+//
+//    }
+//};
+
+String.prototype.bind = function(parameters) {
+    var regexContent,
+        regexHref,
+        regexClass,
+        prop,
+        output = this;
+
+    for (prop in parameters) {
+        regexContent = new RegExp('(<.*?data-bind-content="' + prop + '".*?>)(.*?)(<\\s*/.*?>)', 'g');
+        regexHref = new RegExp('(<.*?data-bind-href="' + prop + '".*?)>', 'g');
+        regexClass = new RegExp('(<.*?data-bind-class="(' + prop + ')".*?)>', 'g');
+
+        output = output.replace(regexContent, function(element, openingTag, content, closingTag) {
+            return openingTag + parameters[prop] + closingTag;
+        });
+
+        output = output.replace(regexHref, function (tag, contentBeforeClosing) {
+            return contentBeforeClosing + ' href="' + parameters[prop] + '">';
+        });
+
+        output = output.replace(regexClass, function (tag, contentBeforeClosing) {
+            return contentBeforeClosing + ' class="' + parameters[prop] + '">';
+        });
+    }
+
+    return output;
+};
 
 function htmlBinding() {
     'use strict';
     var str = '<div data-bind-content="name"></div>',
         output = str.bind({name: 'Steven'});
+    console.log(output);
 
+    str = '<a data-bind-content="name" data-bind-href="link" data-bind-class="name"></à>';
+    output = str.bind({name: 'Elena', link: 'http://telerikacademy.com'});
+    console.log(output);
 }
