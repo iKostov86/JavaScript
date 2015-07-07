e2630ecaff1d38f2bb6287433d6d1d38f0089c9f
iKostov86 <i.kostov86@gmail.com>
2015-07-08 01:53:39 +0300

    arrays methods initial all tasks

---
+++ Array Methods/script.js
@@ -0,0 +1,29 @@
+//Problem 1. Make person
+function makePerson() {
+    'use strict';
+}
+
+//Problem 2. People of age
+function peopleOfAge() {
+    'use strict';
+}
+
+//Problem 3. Underage people
+function underagePeople() {
+    'use strict';
+}
+
+//Problem 4. Average age of females
+function averageAgeOfFemales() {
+    'use strict';
+}
+
+//Problem 5. Youngest person
+function youngestPerson() {
+    'use strict';
+}
+
+//Problem 6. Group people
+function groupPeople() {
+    'use strict';
+}
