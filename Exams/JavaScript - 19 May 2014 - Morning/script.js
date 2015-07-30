024aca831e3f6aa57ee8480459272fb913833e5a
iKostov86 <i.kostov86@gmail.com>
2015-07-30 23:06:39 +0300

    Exams/JavaScript - 19 May 2014 - Morning task 2

--- Exams/JavaScript - 19 May 2014 - Morning/script.js
+++ Exams/JavaScript - 19 May 2014 - Morning/script.js
@@ -22,3 +22,63 @@ function vehicles(args) {
 }
 
 //2. Paths
+function paths() {
+    'use strict';
+    //debugger;
+    var args = [].slice.call(arguments[0]),
+        temp = args[0].split(' '),
+        rows = temp[0],
+        cols = temp[1],
+        matrix = [],
+        used = [],
+        sum = 0,
+        i,
+        j;
+
+    temp = [];
+    matrix.length = rows;
+    used.length = rows;
+    for (i = 0; i < rows; i += 1) {
+        matrix[i] = new Array(cols);
+        used[i] = new Array(cols);
+        temp = args[i + 1].split(' ');
+        for (j = 0; j < cols; j += 1) {
+            matrix[i][j] = temp[j];
+            used[i][j] = undefined;
+        }
+    }
+
+    i = 0;
+    j = 0;
+    //debugger;
+    while(i >= 0 && i < rows && j >= 0 && j < cols) {
+        if(!!used[i][j]) {
+            return 'failed at (' + i + ', ' + j + ')';
+        }
+        used[i][j] = true;
+        sum += Math.pow(2, i) + j;
+        if(matrix[i][j].charAt(0) == 'd' && matrix[i][j].charAt(1) == 'r') {
+            i += 1;
+            j += 1;
+        } else if(matrix[i][j].charAt(0) == 'd' && matrix[i][j].charAt(1) == 'l') {
+            i += 1;
+            j -= 1;
+        } else if(matrix[i][j].charAt(0) == 'u' && matrix[i][j].charAt(1) == 'r') {
+            i -= 1;
+            j += 1;
+        } else {
+            i -= 1;
+            j -= 1;
+        }
+    }
+
+    return 'successed with ' + sum;
+}
+
+console.log(paths([
+        '3 5',
+        'dr dl dr ur ul',
+        'dr dr ul ur ur',
+        'dl dr ur dl ur'
+    ]
+));
