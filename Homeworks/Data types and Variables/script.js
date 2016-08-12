6d5454d024f4e540ff4d36f74d173b80a62f1023
iKostov86 <i.kostov86sync@gmail.com>
2016-08-12 18:42:41 +0300

    renaime file names

---
+++ Homeworks/Data types and Variables/script.js
@@ -0,0 +1,21 @@
+var int = 2;
+var str = 'basi mamata';
+var nothing = null;
+var undefined = undefined;
+var float = 3.666;
+var maxValue = Number.MAX_VALUE;
+var minValue = Number.MIN_VALUE;
+var pi = Math.PI;
+var bool = true;
+
+var quotedText = '\'\'How you doin\'?\', Joey said\'';
+console.log(quotedText);
+
+console.log('the type of "int" is: ' + typeof int);
+console.log('the type of "float" is: ' + typeof float);
+console.log('the type of "str" is: ' + typeof str);
+console.log('the type of "pi" is: ' + typeof pi);
+console.log('the type of "maxValue" is: ' + typeof maxValue);
+console.log('the type of "undefined" is: ' + typeof undef);
+console.log('the type of "bool" is: ' + typeof bool);
+console.log('the type of "null" is: ' + typeof null);
