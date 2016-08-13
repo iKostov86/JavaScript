765cf3f3604a4d92bab4a169b31befae34e752a6
iKostov86 <i.kostov86sync@gmail.com>
2016-08-13 15:40:27 +0300

    some improvements on script.js

--- Homeworks/Loops/script.js
+++ Homeworks/Loops/script.js
@@ -1,61 +1,141 @@
-//Problem 1. Numbers
-function printsNumbers(n) {
+// Problem 1. Numbers
+function printsNumbers(args) {
     'use strict';
-    var i;
+    var num = +args[0];
+    var result = '';
 
-    for(i = 1; i <= n; i += 1 ) {
-        console.log(i);
+    for(var i = 1; i < num; i += 1 ) {
+        result += i + ' ';
     }
+
+    result += i;
+
+    console.log(result);
 }
 
-//Problem 2. Numbers not divisible
+// Problem 2.1 Numbers that are not divisible
 function printsNumberThatAreNotDivisibleBy3And7(n) {
     'use strict';
-    var i;
-
-    for (i = 1; i <= n; i += 1) {
+    for (var i = 1; i <= n; i += 1) {
         if (i % 3 && i % 7) {
             console.log(i);
         }
     }
 }
 
-//Problem 3. Min/Max of sequence
-function minMaxOfSequence() {
+// Problem 2.2 Min/Max/Sum/Average of sequence
+function MMSA() {
     'use strict';
-    var biggest,
-        length,
+    var args = Array.prototype.slice.apply(arguments[0]),
+        max,
+        min,
+        sum,
+        avg,
+        len,
         i;
 
-    biggest = arguments[0];
-    length = arguments.length;
+    max = +args[0];
+    min = +args[0];
+    sum = +args[0];
+    len = args.length;
 
-    for (i = 1; i < length; i += 1) {
-        if (biggest < arguments[i]) {
-            biggest = arguments[i];
+    for (i = 1; i < len; i += 1) {
+        var arg = +args[i];
+
+        if (max < arg) {
+            max = arg;
         }
+        if (min > arg) {
+            min = arg;
+        }
+
+        sum += arg;
     }
 
-    console.log(biggest);
+    avg = sum / len;
+
+    // console.log('min=1.00 max=5.00 sum=8.00 avg=2.67');
+    console.log('min=' + min.toFixed(2));
+    console.log('max=' + max.toFixed(2));
+    console.log('sum=' + sum.toFixed(2));
+    console.log('avg=' + avg.toFixed(2));
 }
 
-//Problem 4. Lexicographically smallest
-function lexicographicallySmallest(element) {
+// MMSA(['2', '5', '1']);
+
+// Problem 3. Matrix of numbers
+function matrixOfNumbers(args) {
+    var num = +args[0];
+
+    // for (var i = 1; i <= num; i += 1) {
+    //     var line = '';
+
+    //     for (var j = i; j < num + i - 1; j += 1) {
+    //         line += j + ' ';
+    //     }
+
+    //     line += j;
+    //     console.log(line);
+    // }
+
+    if (num === 0) {
+        console.log(num);
+    }
+    else {
+        var line = '';
+        var count = 0;
+        for (var i = count + 1; i <= num + count; i += 1) {    
+            if (i >= num * 2) {
+                break;
+            } else if (i === num + count) {
+                line += i;
+                i = ++count;
+                console.log(line);
+                line = '';            
+            } else {
+                line += i + ' ';
+            }
+        }
+    }
+}
+
+// for (var i = 0; i < 5; i += 1) {
+//     matrixOfNumbers([i + '']);
+//     console.log();
+// }
+
+// Problem 4. Lexicographically smallest
+function lexicographicallyTheSmallest(element) {
     'use strict';
-    var i,
-        biggest = 'a',
-        smallest = 'Z',
+    var theLargest = 'A',
+        theSmallest = 'z',
         prop;
 
     for (prop in element) {
-        if (biggest < prop) {
-            biggest = prop;
+        for (var i = 0, len = Math.min(theLargest.length, prop.length); i < len; i += 1) {
+            if (theLargest[i] === prop[i]) {
+                continue;
+            }
+            else if (theLargest[i] < prop[i]) {
+                theLargest = prop;
+            }
+            else {
+                break;
+            }
         }
-        if (smallest > prop) {
-            smallest = prop;
+        for (var i = 0, len = Math.min(theSmallest.length, prop.length); i < len; i += 1) {
+            if (theSmallest[i] === prop[i]) {
+                continue;
+            }
+            else if (theSmallest[i] > prop[i]) {
+                theSmallest = prop;
+            }
+            else {
+                break;
+            }
         }
     }
 
-    console.log('The biggest is: ' + biggest);
-    console.log('The smallest is: ' + smallest);
+    console.log('The largest is: ' + theLargest);
+    console.log('The smallest is: ' + theSmallest);
 }
